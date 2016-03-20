52c1d7af453d843c3c32bc5ab34955098c83a321
Nate <teamtomkins23@gmail.com>
2016-03-19 22:45:01 -0500

    Adds routing specs

--- spec/routing/games_routing_spec.rb
+++ spec/routing/games_routing_spec.rb
@@ -17,5 +17,17 @@ describe GamesController, :type => :routing do
     it 'routes to #update' do
       expect(:put => '/games/1').to route_to('games#update', :id => '1')
     end
+
+    it 'routes to #vote' do
+      expect(:post => '/games/1/vote').to route_to('games#vote', :id => '1')
+    end
+
+    it 'routes to #owned' do
+      expect(:post => '/games/1/owned').to route_to('games#owned', :id => '1')
+    end
+
+    it 'routes to #clear' do
+      expect(:post => '/games/clear').to route_to('games#clear')
+    end
   end
 end
